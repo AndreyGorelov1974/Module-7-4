13c976bb29c534e2c5113b47f989feef8013f2cc
Andrey Gorelov <shorkun1974@yandex.ru>
2022-11-01 18:43:00 +0300

    Добавьте файлы проекта.

---
+++ Module 7-4.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите количество воды в мл: ";
+	int volumeWater;
+	std::cin >> volumeWater;
+
+	while (volumeWater < 0) {
+		std::cout << std::endl << "Количество воды должно быть больше 0. Введите снова: ";
+		std::cin >> volumeWater;
+	}
+}
